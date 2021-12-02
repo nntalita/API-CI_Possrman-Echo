cdde9fd24db98d596c4f6694923dabe308f55105
Natalya Pletneva <nntalita@gmail.com>
2021-12-02 22:33:10 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/RequestTest.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RequestTest {
+    @Test
+    void shouldRequest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("кот") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("котик"));
+    }
+}
